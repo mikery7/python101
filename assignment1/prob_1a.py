157b153b7395f14161c98ecbeb200a05347470c3
Mike Kery <mike.kery@gmail.com>
2018-07-06 18:55:31 +0000

    Completed Exercise 1a

--- assignment1/prob_1a.py
+++ assignment1/prob_1a.py
@@ -1,15 +1,17 @@
 #define function
+
 def print_list(reverse_list):
   '''
    print the input list
   '''
-  print reverse_list
-  #print "{0}: {1}".format(len(reverse_list), reverse_list)
+  rlist = [str(n) for n in reverse_list]
+  print "{0}: {1}".format(len(reverse_list), ', '.join(rlist))
+
 
 def reverse_list(input_list):
   reverse_list = []
-  for num in range(0,5):
-    reverse_list.append(input_list[num])
+  for num in range(1,len(input_list)+1):
+    reverse_list.append(input_list[-num])
   return reverse_list
 
 L1 = [1, 10, 11, 13, 2, 17, 25]
